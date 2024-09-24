e286a28661bb8184acc1cd1ab025897cab1c7947
Monica Rotulo <monica.rotulo@gmail.com>
2024-09-24 11:43:53 +0200

    Update aircraft_online_learning.m

--- aircraft_online_learning.m
+++ aircraft_online_learning.m
@@ -9,12 +9,10 @@ A1c =[-0.2296, 0.9931; 0.02436, -0.2046];
 B1c = [-0.0434 -0.01145; -1.73 -0.517];  
 [A1, B1] = c2d(A1c,B1c, Ts) 
 eigs(A1) 
-check_ctrl(A1,B1) 
  
 A2c = [-1.175 0.9871; -8.458 -0.8776]; 
 B2c = [-0.194, -0.03593; -19.29, -3.803];  
 [A2, B2] = c2d(A2c,B2c, Ts) 
-check_ctrl(A2,B2) 
 eigs(A2) 
  
 A_sw{1}=A1; A_sw{2}=A2;  
@@ -36,16 +34,13 @@ for i =1:T
     x = A1*x+B1*U(:,i); 
     X = [X, x]; 
 end 
- 
-%gam1 = 18.1849 
-%gam2 = 6.1621 
-%[kappa,tau, C, delta, Pi, mu, alpha] = get_bounds2(gam1, gam2, 0.99) 
-% ora ho il dataset offline U_-1, X_-1 and X0 e vado online da k=0 
- 
-%% go online 
+
+
+%% after i got the dataset offline U_-1, X_-1, X_0, now go online from k=0 
 time=150; 
 time_faults = [10; 25;50;67;95;110]; %95 
 sw_seq = [2,1,2,1,2,1]; 
+
 As = A1; Bs = B1; eps= 0.001; N=[]; 
 sw_con=1; count=1; 
 Xcl=[]; Ucl=[]; y_faults=[]; 
@@ -83,7 +78,7 @@ for t=0:time
     if isnan(K) 
         K = old_K; 
     end 
-    %formo la mia u(t) e salvo u(t) e x(t) 
+    %construct my u(t) and save u(t), x(t) 
     e = -eps + (eps+eps).*(randn(1,m))'; 
     %E = [E, e]; 
     u = K*x + norm(x)*e; 
@@ -93,13 +88,13 @@ for t=0:time
         N = [N, norm(x)]; 
         %%%% 
     x = As*x+Bs*u;  
-    %aggiorno la x(t+1) e u(t) per il next step 
+    %update x(t+1) and u(t) for the next step 
     U = [U, u]; 
     X = [X, x]; 
 end 
  
 %% 
-colors = {'#ff9100','#1e96fc' }; % orange 1c77c3 7f95d1 
+colors = {'#ff9100','#1e96fc' }; 
 figure 
 subplot 211 
 box on 
@@ -131,4 +126,4 @@ for j = 1:m
     grid on 
 end 
  
-%save('aircraft_bello')
+%save('aircraft_case')
